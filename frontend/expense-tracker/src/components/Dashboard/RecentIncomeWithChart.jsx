3ac28b9f08bc471d0d84388f1755e40f2eaea093
JP-juniorr <jp.noname@icloud.com>
2025-07-09 17:41:42 +0200

    Fixed

--- frontend/expense-tracker/src/components/Dashboard/RecentIncomeWithChart.jsx
+++ frontend/expense-tracker/src/components/Dashboard/RecentIncomeWithChart.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import CustomPieChart from "../Charts/CustomPieChart";
 
 import { prepareExpenseBarChartData } from "../../utils/helper";
+import CustomPieChart from "../Charts/CustomPieChart";
 
 const COLORS = ["#875CF5", "#FA2C37", "#FF6900", "#4f39f6"];
 
