ac039b056313ceb3d371b2cf746eb0f0a9060d47
JP-juniorr <jp.noname@icloud.com>
2025-07-09 17:35:53 +0200

    s

--- frontend/expense-tracker/src/components/Dashboard/FinanceOverview.jsx
+++ frontend/expense-tracker/src/components/Dashboard/FinanceOverview.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import CustomPieChart from "../Charts/CutomPieChart";
+import CustomPieChart from "../Charts/CustomPieChart";
+
 const COLORS = ["#875CF5", "#FA2C37", "#FF6900"];
 
 const FinanceOverview = ({ totalBalance, totalIncome, totalExpense }) => {
